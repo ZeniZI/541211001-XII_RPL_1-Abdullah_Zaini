958e95809af4b32aabce8c06fc87bd73c47af019
ZeniZI <abdullahzaini90@gmail.com>
2023-11-07 15:26:54 +0700

    Express.js #14 Menambahkan Data

--- controllers/users.js
+++ controllers/users.js
@@ -3,36 +3,39 @@ const User = require('../models/User')
 module.exports = {
     index: async (req, res) => {
         try {
-            users = await User.find()
+            const users = await User.find()
+        if(users.length > 0){
             res.status(200).json({
                 status: true,
                 data: users,
                 method: req.method,
                 url:req.url
             })
-        } catch (error) {
-            res.status(400).json({sucess: false})
-
-        }
-        if(users.length > 0){
-            
         }else{
             res.json({
                 status: false,
                 message: "Data masih kosong"
             })
         }
+    } catch (error) {
+            res.status(400).json({success: false})
+        }
+
       },
-      store: (req, res) => {
-        users.push(req.body)
-        res.json({
-            status: true,
-            data: users,
-            method: req.method,
-            url:req.url,
-            message: "Data berhasil ditambahkan"
-        })
-      },
+      store: async (req, res) => {
+        try {
+            const user = await User.create(req.body)
+            res.status(200).json({
+                status: true,
+                data: user,
+                method: req.method,
+                url: req.url,
+                message: "Data berhasil ditambahkan"
+            })
+        } catch (error) {
+            res.status(400).json({success: false})
+        }
+    },
       update: (req, res) => {
         const id = req.params.id
         users.filter(user =>  {
