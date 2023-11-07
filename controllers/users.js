9c251c32ae1a36f294de180c88c234fa1f38f716
ZeniZI <abdullahzaini90@gmail.com>
2023-11-07 15:52:23 +0700

    Express.js #15 Mengubah Data

--- controllers/users.js
+++ controllers/users.js
@@ -36,23 +36,24 @@ module.exports = {
             res.status(400).json({success: false})
         }
     },
-      update: (req, res) => {
-        const id = req.params.id
-        users.filter(user =>  {
-            if(user.id == id){
-                user.nama = req.body.nama
-                user.email = req.body.email
-                return user
-            }
-        })
-        res.json({
-            status: true,
-            data: users,
-            method: req.method,
-            url:req.url,
-            message: "Data berhasil diubah"
-        })
-      },
+      update: async (req, res) => {
+        try {
+            const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+                new: true,
+                runValidators: true
+            })
+            res.json({
+                status: true,
+                data: user,
+                method: req.method,
+                url:req.url,
+                message: "Data berhasil diubah"
+            })
+        } catch (error){
+            res.status(400).json({success: false})
+        }
+         const id = req.params.id
+        },
       delete: (req, res) => {
         const id = req.params.id
         users = users.filter(user => user.id != id)
